49af236802b8eb4ae95bf2f95cc5a13a41365265
Raul <raul.pedraza@ikasle.egibide.org>
2019-10-21 10:37:16 +0200

    Reto 2

--- DESARROLLO-WEB-EN-ENTORNO-CLIENTE-master 3/Practica 1/Practica1Ejercicio15.js
+++
@@ -1,4 +0,0 @@
-let n1=prompt('Introduzca el primer numero')
-let n2=prompt('Introduzca el segundo numero')
-alert('la resta es: '+(n1-n2))
-//muestra como mensaje NaN si son textos
--- DESARROLLO-WEB-EN-ENTORNO-CLIENTE-master 3/Practica 1/Practica1Ejercicio24.js
+++
@@ -1,3 +0,0 @@
-var n= parseInt(prompt('introduce el numero a factorizar'))
-for(x=n-1;x>0;x--)n=n*x
-alert(' factorizado es '+n)
